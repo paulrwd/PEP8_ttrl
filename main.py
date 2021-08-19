cd085a936be628f7b1cf0881041e45a99c67b330
Pavel Mitkalev <paulrwd85@gmail.com>
2021-08-19 14:29:34 +0300

    210819_1

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 # -*- coding: utf-8 -*-
 """Utilities for writing code that runs on Python 2 and 3"""
 
-print('hello1')
+prince = 9_9
+print(prince)
+
+a = 0o11
+b = 2.0
+c = a + b
+d = True
+e = 3
+f = a / b
+g = bin(a)
+
+print(type(a), type(b), type(c), c, type(d), type(f), type(g), g, a)
