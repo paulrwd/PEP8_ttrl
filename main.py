c6b3393c6b2005fbb161664dfa97f89be1e0d21a
Pavel Mitkalev <paulrwd85@gmail.com>
2021-08-20 11:13:06 +0300

    210820_1

--- main.py
+++ main.py
@@ -1,15 +1,8 @@
 # -*- coding: utf-8 -*-
 """Utilities for writing code that runs on Python 2 and 3"""
 
-prince = 9_9
-print(prince)
-
-a = 0o11
-b = 2.0
-c = a + b
-d = True
-e = 3
-f = a / b
-g = bin(a)
-
-print(type(a), type(b), type(c), c, type(d), type(f), type(g), g, a)
+count = 1
+while count <= 5:
+    print(count)
+    count += 1
+print(count)
